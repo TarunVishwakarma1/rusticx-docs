26cee03775d2c28e75ca483f618b0955f84f16ba
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2025-05-17 01:50:49 +0530

    added

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+import nextra from 'nextra'
+ 
+// Set up Nextra with its configuration
+const withNextra = nextra({
+
+})
+ 
+// Export the final Next.js config with Nextra included
+export default withNextra({
+    turbopack:{
+        resolveAlias:{
+            'next-mdx-import-source-file': './src/mdx-components.js'
+        }
+    }
+})
